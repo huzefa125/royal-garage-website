fd00e0683c5cc955ca2de7606a20fe04808fd0a2
Shaikh Huzef <huzafahuzafa63@gmail.com>
2026-06-24 18:56:35 +0530

    Add FAQ schema and sitemap integration; update site URL and create robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // Import the Vercel adapter
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://positivustheme.vercel.app",
-  integrations: [tailwind()],
+  site: "https://royal-garage-website-t93y.vercel.app",
+  integrations: [tailwind(), sitemap()],
   output: 'server',
   adapter: vercel(),
 });
